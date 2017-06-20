bd4799bff18cc62fd15e65bcdb59b163b32bde3d
tmahmadou01 <mahamadoutraore1@gmail.com>
2017-06-20 15:04:05 +0200

    Installation de FosUserBundle[1]

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,11 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+
+            // ...
+            new FOS\UserBundle\FOSUserBundle(),
+            // ...,
+            new MarmitonBundle\MarmitonBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
